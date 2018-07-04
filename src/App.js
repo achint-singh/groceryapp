b39c0ca8bb218575ff54e5961cdca6f7ad4181a0
Achint Singh <achintsingh97@gmail.com>
2018-07-04 16:45:11 -0700

    Added simple search

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <form action="">
+          <input type="text" placeholder="Enter food item here"/>
+          <input type="submit" value="Submit"/>
+        </form>
+
       </div>
     );
   }
