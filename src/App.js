564f62d45afa0e02da21e1add08b1533a0ac1204
Achint Singh <achintsingh97@gmail.com>
2018-07-04 17:17:57 -0700

    Added event handlers to search

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      foodItem: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log("A search was submitted");
+    console.log(this.state.foodItem);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({foodItem: event.target.value});
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -14,8 +35,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <form action="">
-          <input type="text" placeholder="Enter food item here"/>
+        <form id="search" action="" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.foodItem} onChange={this.handleChange} placeholder="Enter food item here"/>
           <input type="submit" value="Submit"/>
         </form>
 
