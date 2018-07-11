f45c85ac0d22b4f8e31669d2e3f017db140653a1
Hillary Fan <hillary.fan123@gmail.com>
2018-07-10 19:26:59 -0700

    added vegan checkbox filter and some logic, bad request error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
+
+// App.PropTypes = {
+//   healthParam: PropTypes.oneOf(['vegan', 'vegetarian'])
+// }
+
+// const healthParam = {
+//   vegan: 'vegan',
+//   vegetarian: 'vegetarian'
+// }
 
 class App extends Component {
   constructor() {
@@ -8,35 +18,62 @@ class App extends Component {
     this.state = {
       foodItem: "",
       relatedItems: [],
+      healthFilter: "",
       appID: "1a53aa6a",
       appKey: "4dede038213c6fa35890d3f97c6c82ae",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateHealth = this.updateHealth.bind(this);
+
   }
   
-   getFoodItemsFromApi(item) {
-    return fetch('https://api.edamam.com/api/food-database/parser?ingr='+item+'&app_id='+this.state.appID+'&app_key='+this.state.appKey)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({relatedItems: responseJson.hints});
-        console.log(this.state.relatedItems);
-        return responseJson.hints;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+   getFoodItemsFromApi() {
+    if (this.state.healthFilter !== "") { // health filter applied
+      return fetch('https://api.edamam.com/api/food-database/parser?ingr='+this.state.foodItem+'&app_id='+this.state.appID+'&app_key='+this.state.appKey+'&health='+this.state.healthFilter)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          this.setState({relatedItems: responseJson.hints});
+          console.log(this.state.relatedItems);
+          return responseJson.hints;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else { // no health filter applied
+        return fetch('https://api.edamam.com/api/food-database/parser?ingr='+this.state.foodItem+'&app_id='+this.state.appID+'&app_key='+this.state.appKey)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          this.setState({relatedItems: responseJson.hints});
+          console.log(this.state.relatedItems);
+          return responseJson.hints;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   }
 
   handleSubmit(event) {
     console.log("A search was submitted" + this.state.foodItem);
-    this.getFoodItemsFromApi(this.state.foodItem)
+    this.getFoodItemsFromApi();
     event.preventDefault();
   }
 
   handleChange(event) {
     this.setState({foodItem: event.target.value});
+  }
+
+  updateHealth(event) {
+    if (event.target.checked) {
+      this.setState({healthFilter: event.target.value});
+    } else {
+      this.setState({healthFilter: ""});
+    }
+
   }
 
   render() {
@@ -52,6 +89,10 @@ class App extends Component {
 
         <form id="search" action="" onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.foodItem} onChange={this.handleChange} placeholder="Enter food item here"/>
+          <div>
+            <input type="checkbox" name="isVegan" value="vegan" onChange={this.updateHealth}/>
+            <label htmlFor="isVegan">Vegan</label>
+          </div>
           <input type="submit" value="Submit"/>
         </form>
 
